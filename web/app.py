8118da3cf44f727c04ac7cf217e01a9ed020df7d
Hugo DOMAGÉ <91956673+hugo-domage@users.noreply.github.com>
2023-12-11 14:37:31 +0100

    Add files via upload

---
+++ web/app.py
@@ -0,0 +1,19 @@
+from flask import request, Flask
+import json
+
+app = Flask(__name__)
+
+@app.route('/plus_one')
+def plus_one():
+  x = int(request.args.get('x', 1))
+  return json.dumps({'x': x + 1})
+  
+@app.route('/plus_two')
+def plus_two():
+  x = int(request.args.get('x', 1))
+  return json.dumps({'x': x + 2})
+  
+@app.route('/square')
+def square():
+  x = int(request.args.get('x', 1))
+  return json.dumps({'x': x * x}) 
